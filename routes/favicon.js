d404c48abbd107e5a5685dd5876c99621007bf2e
DamareonCarter <carted92@gmatc.matc.edu>
2024-12-03 04:45:22 -0600

    Added variable page

--- routes/favicon.js
+++ routes/favicon.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, './public/images/js_logo.png'));
+    res.sendFile(path.join(import.meta.dirname, '../public/images/js_logo.png'));
 });
 
 export default router;
